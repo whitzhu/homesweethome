f7035071d27d6f76277d2ab2480ab0f3e47cdec3
whitzhu <whitzhu@gmail.com>
2017-06-07 00:18:22 -0700

    setup react and basic express server

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      test: /\.(jsx|js)?$/,
+      loaders: ['babel-loader'],
+      include: SRC_DIR,
+    }],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    inline: true,
+    contentBase: DIST_DIR,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
