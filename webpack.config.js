302516709574cd5a4999c8b29a570a21f797fe2d
whitzhu <whitzhu@gmail.com>
2017-06-07 14:58:24 -0700

    add sass-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const SRC_DIR = path.join(__dirname, '/src');
 const DIST_DIR = path.join(__dirname, '/public');
@@ -21,10 +22,20 @@ module.exports = {
       test: /\.(jsx|js)?$/,
       loaders: ['babel-loader'],
       include: SRC_DIR,
-    }],
+    },
+    {
+      test: /\.(sass|scss)$/,
+      loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+    },
+    { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader?name=[name].[ext]' },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true,
+    }),
   ],
   devServer: {
     inline: true,
