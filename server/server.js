00ab654d896de1b8433e12ddc374a280160003eb
whitzhu <whitzhu@gmail.com>
2017-06-09 16:20:56 -0700

    [favicon] add express favicon loader and simple favicon icon

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(process.env.PORT || 3000, () => {
